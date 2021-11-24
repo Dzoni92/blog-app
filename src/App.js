063d63126d82ef286e86149cc6230c2af1ff2975
unknown <nikola.josipovic369@gmail.com>
2021-11-24 18:44:00 +0100

    add mui and button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello</h2>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
